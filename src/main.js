cd6c4467e931245370b29763e1c7c2d442d09130
Razieh Kazeminejad <r.kazeminejad98@gmail.com>
2022-08-21 12:27:47 +0430

    optimize code (using asynchronus components)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
-import BaseSpinner from './components/ui/BaseSpinner.vue'
-import BaseDialog from './components/ui/BaseDialog.vue'
-import BasePrice from './components/ui/BasePrice.vue'
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+import BasePrice from './components/ui/BasePrice.vue';
+
+const BaseDialog = defineAsyncComponent(() =>
+  import('./components/ui/BaseDialog.vue')
+);
 
 import App from './App.vue';
 import router from './router';
